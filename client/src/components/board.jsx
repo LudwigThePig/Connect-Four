d46935aeaf3e2dee4694dcd51be219980f078d73
Morgan Galvin <morgan@ludwigthepig.com>
2019-05-09 11:48:55 -0600

    Added hover handler

--- client/src/components/board.jsx
+++ client/src/components/board.jsx
@@ -5,21 +5,55 @@ class Board extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-      discs: Array.apply(null, Array(42)).map( (x, i) =>  i) 
+      discs: Array.apply(null, Array(42)).map( (x, i) =>  undefined) 
     }
     this.indexToCoord = this.indexToCoord.bind(this);
+    this.handleHover = this.handleHover.bind(this);
+    this.dropPiece = this.dropPiece.bind(this);
   }
+
   indexToCoord(index) {
-    const x = Math.floor(index / 7); 
-    const y = index % 7;
+    const x = index % 7;
+    const y = Math.floor(index / 7);
     return {x: x, y: y}
   }
+
+  // handleHover = (e) => {
+  //   console.log(e);
+  // }
+
+  // dropPiece = (e) => {
+  //   console.log(e);
+  // }
+  handleHover(e) {
+    const xCoord = e.target.dataset.x;
+    let cols = [...document.getElementsByClassName('disc')].filter(disc => disc.dataset.x === xCoord)
+    return e.type === 'mouseover' ? this.handleMouseOver(cols) : this.handleMouseOut(cols);
+  }
+
+  handleMouseOver(list) {
+    return [...list].forEach(el => el.classList.add('hovered'));
+  }
+  
+  handleMouseOut(list) {
+    return [...list].forEach(el => el.classList.remove('hovered'));
+  }
+
+
+  dropPiece(e) {
+    // console.log(e);
+  }
+
   render() {
     console.log(this.state.discs)
     return (
       <div id="board">
         {this.state.discs.map( (disc, index) => 
-          <Disc coords={this.indexToCoord(index)} />
+          <Disc 
+            coords={this.indexToCoord(index)} 
+            handleHover={this.handleHover}
+            dropPiece={this.dropPiece}
+          />
         )}
       </div>
     )
--- client/src/components/disc.jsx
+++ client/src/components/disc.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 const Disc = (props) => {
   return (
-    <div className="disc" data-x={props.coords.x}  data-y={props.coords.y} data-coord>
+    <div className="disc" 
+      data-x={props.coords.x}  
+      data-y={props.coords.y} 
+      onMouseOver={props.handleHover} 
+      onMouseOut={props.handleHover}
+      onClick={props.dropPiece}
+    >
     </div>
   )
 }
